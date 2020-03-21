52d503a793c757c02adedae28593d02e21bc3bde
carloserlemus <carlosriveralemus@gmail.com>
2020-03-20 18:00:43 -0700

    Added js files, updated notes, installed dependancies.

---
+++ app.js
@@ -0,0 +1,6 @@
+// import express package
+const express = require('express');
+// execute express so we can use express stuff!
+const app = express();
+// use sets up a middleware.. and incoming request has to go through it
+app.use()
---
+++ server.js
@@ -0,0 +1,22 @@
+// All the code to use node.js server
+
+/* Importing http package to give us the functionality
+we require for using a server */
+const http = require('http');
+
+// dynamic port constant.
+const port = process.env.PORT || 3000;
+
+/* in the createServer http command we need to pass a LISTENER
+A FUNCTION which is executed every time we have a new request and
+returns a Response. */
+const server = http.createServer()
+
+// Start the server.
+server.listen(port, (err) => {
+    if (err) {
+        console.log('Something bad happened')
+    } else {
+        console.log('Connect to port: ', port)
+    }
+})

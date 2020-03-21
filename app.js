a3a894f2fef011c87647cf51bbf5a54e915a6687
carloserlemus <carlosriveralemus@gmail.com>
2020-03-21 04:55:22 -0700

    added body parser

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 // import express package
 const express = require('express');
 
+// import body parser.
+
+const bodyParser = require('body-parser');
+
 // execute express so we can use express stuff!
 const app = express();
-// Why Morgan? [ERROR HANDLING]
+// Morgan: Logging Package. To log incoming requests.
 const morgan = require('morgan');
+
 // import productRoutes
-
-
 // REQUESTS are forwarded to product.js if the URL they are targetting 
 // ends with /products
 //Routes that handle requests.
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Why is it important to use() before the routes?
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 // use sets up a middleware.. and incoming request has to go through app.use
 // and whatever we pass to it.
 
 app.use('/products', productRoutes); // Anything in the url that has /products will forward to products.js
 app.use('/orders', orderRoutes);
+
+// Error Handling
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);    // Forwards to the next error...
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;

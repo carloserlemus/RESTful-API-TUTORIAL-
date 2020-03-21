325e6117c6607598854ca74e632ca8c77c19440f
carloserlemus <carlosriveralemus@gmail.com>
2020-03-20 20:31:16 -0700

    Added Morgan dependancy.

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require('express');
 
 // execute express so we can use express stuff!
 const app = express();
-
+// Why Morgan? [ERROR HANDLING]
+const morgan = require('morgan');
 // import productRoutes
 
 
 // REQUESTS are forwarded to product.js if the URL they are targetting 
 // ends with /products
+//Routes that handle requests.
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // use sets up a middleware.. and incoming request has to go through app.use
 // and whatever we pass to it.
